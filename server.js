5fe6e381c4236dc35ec51d7b494d4cc7e7cc9078
Tim <blargendarg@gmail.com>
2016-03-03 18:16:18 +0000

    get basic url param search working and into res

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ var express = require('express'),
     Search = require('bing.search'),
     util = require('util');
     
-    require('dotenv').load();
+require('dotenv').load();
     
-    var search = new Search(process.env.BING_KEY);
+var search = new Search(process.env.BING_KEY);
 
-search.web('Tutta Bella Neapolitan Pizzeria',
-  {top: 5},
-  function(err, results) {
-    if(err) { console.log(err); }
-    console.log(util.inspect(results, 
-      {colors: true, depth: null}));
-  }
-);
-    
+app.use('/*', function(req, res) {
+    console.log(req.params['0']);
+    if(req.params['0'] !== "favicon.ico" && req.params['0'] !== "") {
+        
+        search.web(req.params['0'],
+          {top: 5},
+          function(err, results) {
+            if(err) { console.log(err); }
+              var response = util.inspect(results, 
+              {colors: true, depth: null});
+              res.send(response);
+            }
+        );
+    }
+});
+
+
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
