15e1e5885efa4924a329d1f6a6005bf096fcc536
Tim <blargendarg@gmail.com>
2016-03-03 18:01:14 +0000

    get bing api handler working

--- server.js
+++ server.js
@@ -1,33 +1,18 @@
-'use strict';
+var express = require('express'),
+    app = express(),
+    Search = require('bing.search'),
+    util = require('util');
+    
+    require('dotenv').load();
+    
+    var search = new Search(process.env.BING_KEY);
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
-var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
-
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});
+search.web('Tutta Bella Neapolitan Pizzeria',
+  {top: 5},
+  function(err, results) {
+    if(err) { console.log(err); }
+    console.log(util.inspect(results, 
+      {colors: true, depth: null}));
+  }
+);
+    
